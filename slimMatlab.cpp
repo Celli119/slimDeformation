7fabcff127db53c4f3286a79f988b1717bbda347
girinon <francois.girinon@ensam.eu>
2018-03-13 18:31:48 +0100

    add lim solver

--- slimMatlab.cpp
+++ slimMatlab.cpp
@@ -19,7 +19,7 @@
 #include <igl/matlab/prepare_lhs.h>
 #include <igl/matlab/parse_rhs.h>
 #include <igl/matlab/validate_arg.h>
-
+#include <igl/lim/lim.h>
 
 void mexFunction(
         int nlhs, mxArray *plhs[],
@@ -33,13 +33,15 @@ void mexFunction(
     std::streambuf *outbuf = std::cout.rdbuf(&mout);
     mexErrMsgTxt(nrhs >= 4,"Four arguments expected");
 
-    Eigen::MatrixXd V,U0,U,bc;
+    Eigen::MatrixX3d V,U0,U,bc;
     Eigen::MatrixXi F;
     Eigen::VectorXi b;
 
     int iters = 100;
     double p = 1e5;
-
+    std::string solver;
+    enum solverType {SLIM, LIM};
+    solverType type(LIM);
 
     igl::matlab::parse_rhs_double(prhs+0,V);
     igl::matlab::parse_rhs_index(prhs+1,F);
@@ -69,6 +71,19 @@ void mexFunction(
             igl::matlab::validate_arg_scalar(i,nrhs,prhs,name);
             igl::matlab::validate_arg_double(i,nrhs,prhs,name);
             iters = (double)*mxGetPr(prhs[++i]);
+        }else if(strcmp("Solver",name) == 0)
+        {
+            igl::matlab::validate_arg_char(i,nrhs,prhs,name);
+            solver = mxArrayToString(prhs[++i]);
+            if (std::strcmp(solver.data(), "LIM") == 0){
+                type = LIM;
+            }else if (std::strcmp(solver.data(), "SLIM") == 0){
+                type = SLIM;
+            }
+            else{
+                type = LIM;
+            }
+
         }else if(strcmp("DimCons",name) == 0)
         {
             igl::matlab::validate_arg_double(i,nrhs,prhs,name);
@@ -82,15 +97,64 @@ void mexFunction(
     }
 
 
+    auto V_0 = V;
+    if (type == SLIM){
+        igl::SLIMData sData;
+        sData.exp_factor = 5.0;
+        slim_precompute(V,F,V_0,sData,igl::SLIMData::EXP_CONFORMAL,b,bc,dimCons,p);
+        slim_solve(sData,iters);
+        U = sData.V_o;
+    }
+    else
+    {
+        std::vector<Eigen::Triplet<double>> triplets;
+        triplets.reserve(bc.rows() * 3);
+        std::vector<double> constraintTargetsTmp; constraintTargetsTmp.reserve(V_0.rows() * 3);
+        int maxRow = -1;
+        for (int iConstrain = 0; iConstrain < b.rows(); ++iConstrain)
+        {
+            auto iVertex = b(iConstrain);
+            for (int iDim = 0; iDim < 3; ++iDim) {
+                if (dimCons(iVertex, iDim)){
+                    auto idConstrain = 3 * iVertex + iDim;
+                    if(maxRow < idConstrain)
+                        maxRow = idConstrain;
+                    constraintTargetsTmp.push_back(bc(iConstrain, iDim));
+                    triplets.emplace_back(idConstrain,idConstrain,1);
+                }
+            }
+        }
+        auto constraintTargets = Eigen::VectorXd::Map(constraintTargetsTmp.data(), constraintTargetsTmp.size());
 
-    igl::SLIMData sData;
-    Eigen::MatrixXd V_0 = V;
+        Eigen::SparseMatrix<double> matConstraints;
+        matConstraints.resize(maxRow + 1, V_0.rows() * 3);
+        matConstraints.setFromTriplets(triplets.begin(), triplets.end());
+        auto energy = igl::lim::Green;
+        std::cout << matConstraints.size() << std::endl;
+        U = V_0;
+        std::vector<int> borderVertices;
+        Eigen::VectorXd gradients;
+        auto state = ComputeLIM(
+                U,
+                V_0,
+                F,
+                borderVertices,
+                gradients,
+                matConstraints,
+                constraintTargets,
+                (EnergyType)energy,
+                1e-10,
+                iters,
+                true,
+                true,
+                true,
+                true,
+                -1,
+                -1
+        );
 
-    sData.exp_factor = 5.0;
-    slim_precompute(V,F,V_0,sData,igl::SLIMData::EXP_CONFORMAL,b,bc,dimCons,p);
-    slim_solve(sData,iters);
+    }
 
-    U = sData.V_o;
 
 
     switch(nlhs)
